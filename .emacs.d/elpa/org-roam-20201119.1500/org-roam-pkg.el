1947d825893b6d3aede55067373ece1ceea859ba
ofsahof <ofsahof@gmail.com>
2020-11-22 19:17:09 +0300

    part 1

---
+++ .emacs.d/elpa/org-roam-20201119.1500/org-roam-pkg.el
@@ -0,0 +1,18 @@
+(define-package "org-roam" "20201119.1500" "Roam Research replica with Org-mode"
+  '((emacs "26.1")
+    (dash "2.13")
+    (f "0.17.2")
+    (s "1.12.0")
+    (org "9.3")
+    (emacsql "3.0.0")
+    (emacsql-sqlite3 "1.0.2"))
+  :commit "1b598a461849318fca2df3917c254d8ee483a87e" :keywords
+  ("org-mode" "roam" "convenience")
+  :authors
+  (("Jethro Kuan" . "jethrokuan95@gmail.com"))
+  :maintainer
+  ("Jethro Kuan" . "jethrokuan95@gmail.com")
+  :url "https://github.com/org-roam/org-roam")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
